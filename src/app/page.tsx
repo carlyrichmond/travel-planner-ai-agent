a21820f84752d07a8bb07de0e111f3e1c2f82ace
Carly Richmond <74931905+carlyrichmond@users.noreply.github.com>
2025-02-17 16:08:41 +0000

    Adding useful comments to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,32 @@
 'use client';
 
-//import { Weather } from '@/app/components/weather';
 import { useChat } from '@ai-sdk/react';
 import Spinner from './components/spinner';
 
 export default function Chat() {
+  /* useChat hook helps us handle the input, resulting messages, and also handle the loading and error states for a better user experience */
   const { messages, input, handleInputChange, handleSubmit, isLoading, stop, error, reload } = useChat();
 
   return (
     <div className="chat__form">
       <div className="chat__messages">
-        {messages.map(m => (
+        {
+          /* Display all user messages and assistant responses */
+          messages.map(m => (
           <div key={m.id} className="message">
             <div>
-              <div className="role font-bold">{m.role === "assistant" ? "Sorley" : "Me"}</div>
+              { /* Messages with the role of *assistant* denote responses from the LLM*/ }
+              <div className="role">{m.role === "assistant" ? "Sorley" : "Me"}</div>
+              { /* User or LLM generated content */ }
               <p>{m.content}</p>
             </div>
           </div>
         ))}
       </div>
 
-      {isLoading && (
+      {
+        /* Spinner shows when awaiting a response */
+        isLoading && (
         <div className="spinner__container">
           <Spinner />
           <button id="stop__button" type="button" onClick={() => stop()}>
@@ -29,7 +35,9 @@ export default function Chat() {
         </div>
       )}
 
-      {error && (
+      {
+      /* Show error message and return button when something goes wrong */
+      error && (
         <>
           <div className="error__container">Unable to generate a plan. Please try again later!</div>
           <button id="retry__button" type="button" onClick={() => reload()}>
@@ -38,6 +46,7 @@ export default function Chat() {
         </>
       )}
 
+      { /* Form using default input and submission handler form the useChat hook */ }
       <form onSubmit={handleSubmit}>
         <input
           className="search-box__input"
