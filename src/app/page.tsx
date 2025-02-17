ae8b3d5d5ba2654e1e244c1d890b43d9b33259ad
Carly Richmond <74931905+carlyrichmond@users.noreply.github.com>
2025-02-17 15:40:53 +0000

    Adding spinner and retry button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,14 @@
 
 //import { Weather } from '@/app/components/weather';
 import { useChat } from '@ai-sdk/react';
+import Spinner from './components/spinner';
 
 export default function Chat() {
-  const { messages, input, handleInputChange, handleSubmit } = useChat();
+  const { messages, input, handleInputChange, handleSubmit, isLoading, stop, error, reload } = useChat();
 
   return (
-    <div className="chat-form">
-      <div className="chat-messages">
+    <div className="chat__form">
+      <div className="chat__messages">
         {messages.map(m => (
           <div key={m.id} className="message">
             <div>
@@ -19,12 +20,31 @@ export default function Chat() {
         ))}
       </div>
 
+      {isLoading && (
+        <div className="spinner__container">
+          <Spinner />
+          <button id="stop__button" type="button" onClick={() => stop()}>
+            Stop
+          </button>
+        </div>
+      )}
+
+      {error && (
+        <>
+          <div className="error__container">Unable to generate a plan. Please try again later!</div>
+          <button id="retry__button" type="button" onClick={() => reload()}>
+            Retry
+          </button>
+        </>
+      )}
+
       <form onSubmit={handleSubmit}>
         <input
           className="search-box__input"
           value={input}
           placeholder="Where would you like to go?"
           onChange={handleInputChange}
+          disabled={error != null}
         />
       </form>
     </div>
